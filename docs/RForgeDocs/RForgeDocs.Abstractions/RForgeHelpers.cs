f38aacbefcb4de89cdd96990665b24c08f8f0a74
addisonjm <addisonjm@tamu.edu>
2024-04-18 09:46:50 -0500

    Adding more stuff related to examples.

---
+++ docs/RForgeDocs/RForgeDocs.Abstractions/RForgeHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RForgeDocs.Abstractions;
+public static class RForgeHelpers
+{
+
+    public const string RForgeDocsRepoUrl = "https://github.com/Rumhyek/RForge/tree/main/docs/RForgeDocs/RForgeDocs";
+
+    public static string RepoDocsUrl(string filePath)
+    {
+        if (filePath.StartsWith('/') == false)
+            return $"{RForgeDocsRepoUrl}/{filePath}";
+
+        return $"{RForgeDocsRepoUrl}{filePath}";
+    }
+
+}
