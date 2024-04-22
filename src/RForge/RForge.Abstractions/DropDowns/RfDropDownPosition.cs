5fc199e8dfa22df935b54833bec7335b989ef245
addisonjm <addisonjm@tamu.edu>
2024-04-22 14:48:39 -0500

    Seperating files.

--- src/RForge/RForge.Abstractions/DropDowns/RfDropDownPosition.cs
+++ src/RForge/RForge.Abstractions/DropDowns/RfDropDownPosition.cs
@@ -10,19 +10,3 @@ public enum RfDropDownPosition
     LeftUp,
     RightUp,
 }
-
-public enum RfShowSelectionInDropDown
-{
-    /// <summary>
-    /// Only show the selected item at the top when it is not in the list already.
-    /// </summary>
-    OnlyWhenNotInList,
-    /// <summary>
-    /// The selected item will always render at the top of the drop down even if it is already one of the options shown in the drop down.
-    /// </summary>
-    AlwaysAtTop,
-    /// <summary>
-    /// Never show the selected item at the top. Only show it when it is an option to select from.
-    /// </summary>
-    Never,
-}
---
+++ src/RForge/RForge.Abstractions/DropDowns/RfShowSelectionInDropDown.cs
@@ -0,0 +1,20 @@
+﻿namespace RForge.Abstractions.DropDowns;
+
+/// <summary>
+/// If the drop down has a selection when should it render it at the top of the drop down options.
+/// </summary>
+public enum RfShowSelectionInDropDown
+{
+    /// <summary>
+    /// Only show the selected item at the top when it is not in the list already.
+    /// </summary>
+    OnlyWhenNotInList,
+    /// <summary>
+    /// The selected item will always render at the top of the drop down even if it is already one of the options shown in the drop down.
+    /// </summary>
+    Always,
+    /// <summary>
+    /// Never show the selected item at the top. Only show it when it is an option to select from.
+    /// </summary>
+    Never,
+}
