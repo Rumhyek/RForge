074fbf0bf6f847ff7f01e511faa33d7532a09075
addisonjm <addisonjm@tamu.edu>
2024-10-30 10:52:27 -0500

    Trying to get deployment to work correctly for Azure.

--- docs/RForgeDocs/RForgeDocs/RForgeDocs/Program.cs
+++ docs/RForgeDocs/RForgeDocs/RForgeDocs/Program.cs
@@ -28,7 +28,10 @@ else
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+if(app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseStaticFiles();
 app.UseAntiforgery();
