da558ed554d8901ffbcb3c4c241b1c080ae985b8
Josh Addison <joshaddison396@gmail.com>
2025-11-20 22:46:08 -0600

    Add .NET 10.0 support and enhance Razor components
    Updated target frameworks in multiple projects to include .NET 10.0.

--- src/RForge/RForgeTest/RForgeTest/Program.cs
+++ src/RForge/RForgeTest/RForgeTest/Program.cs
@@ -6,6 +6,7 @@ using RForgeTest.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
